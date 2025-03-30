99f01cb480ab01c603e4f2b673ac4fe8072236af
akitorahayashi <atora.has75@gmail.com>
2025-03-30 16:17:46 +0900

    QRItemCardのデザインを整えた

--- lib/screen/component/qr_item_card.dart
+++ lib/screen/component/qr_item_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../../model/qr_item.dart';
 
@@ -11,73 +12,67 @@ class QRItemCard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // タップ状態を追跡
+    final isPressed = useState(false);
+
     return GestureDetector(
       onTap: () {
         // ディテール画面へ遷移
         context.go('/qr/${item.id}');
       },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: CupertinoColors.systemBackground,
-          boxShadow: [
-            BoxShadow(
-              color: CupertinoColors.systemGrey4.withOpacity(0.2),
-              offset: const Offset(0, 2),
-              blurRadius: 6,
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            children: [
-              // 絵文字の表示
-              Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: CupertinoColors.systemGrey6,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Center(
-                  child: Text(item.emoji, style: const TextStyle(fontSize: 28)),
-                ),
-              ),
-              const SizedBox(width: 12),
-              // テキスト部分
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item.title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      item.url,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: CupertinoColors.secondaryLabel,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Icon(
-                CupertinoIcons.chevron_right,
-                color: CupertinoColors.systemGrey2,
-                size: 20,
+      onTapDown: (_) => isPressed.value = true,
+      onTapUp: (_) => isPressed.value = false,
+      onTapCancel: () => isPressed.value = false,
+      child: AnimatedOpacity(
+        duration: const Duration(milliseconds: 100),
+        opacity: isPressed.value ? 0.6 : 1.0,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: CupertinoColors.systemBackground,
+            border: Border.all(color: CupertinoColors.systemGrey5, width: 1.0),
+            boxShadow: [
+              BoxShadow(
+                color: CupertinoColors.systemGrey3.withOpacity(0.3),
+                offset: const Offset(0, 3),
+                blurRadius: 8,
+                spreadRadius: 1,
               ),
             ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 絵文字の表示
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Center(
+                    child: Text(
+                      item.emoji,
+                      style: const TextStyle(fontSize: 32),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                // タイトル
+                Text(
+                  item.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
